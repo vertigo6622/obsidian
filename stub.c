942aa30a6d57ae532a996760c89a78042187858c
vertigo <181855463+vertigo6622@users.noreply.github.com>
2026-03-19 04:37:52 +0000

    Update stub.c

--- stub.c
+++ stub.c
@@ -1,3 +1,30 @@
+*
+*   __  _  ____ __  __ __ __  ____  ____ _____ __  _______  _____  ____ _____ 
+*  |  \| || ===|\ \/ /|  |  |(_ (_`/ (__`| () )\ \/ /| ()_)|_   _|| ===|| () )
+*  |_|\__||____|/_/\_\ \___/.__)__)\____)|_|\_\ |__| |_|     |_|  |____||_|\_\ v8
+*   x64 pe packer - signal: vertigo.66
+*
+*   License: CC-BY-NC-SA 4.0
+*    
+*   Features:
+*   * BYOS (bring your own stub)
+*   * stub template available
+*   * extensive debug output (-DDEBUG & --debug flags)
+*   * randomized config marker
+*   * zeroed out optional headers
+*   * secure key generation
+*   * checksum recalculation
+*   * pe section manipulation
+*
+*   Compile:
+*   .\gcc.exe stub.c -o stub.o [-DDEBUG] -fno-asynchronous-unwind-tables -fno-ident -fno-stack-protector
+*   .\ld.exe stub.o -o stub.exe -nostdlib --build-id=none -s --entry=_start
+*   .\objcopy.exe -O binary stub.exe stub.bin
+*   .\windres.exe resource.rc -o resource.o 
+*   .\gcc.exe nexus-crypter.c resource.o -o nexus-crypter.exe -lbcrypt
+*
+*/
+
 #include <windows.h>
 #include <stdint.h>
 #include <string.h>
